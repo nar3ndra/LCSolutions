ec989f69d2f9705934f8c4008dd399b7b012578d
nar3ndra <47317679+nar3ndra@users.noreply.github.com>
2020-04-23 04:12:22 +0530

    Solved MaxConsecutiveOnes

---
+++ MaxConsecutiveOnes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count=0;
+        int finalCount=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==1){
+                count++;
+                if(count>finalCount) finalCount=count;
+            }
+            else{
+                count=0;
+            }
+        }
+    return finalCount;
+    }
+}
