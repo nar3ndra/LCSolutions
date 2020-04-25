0314c76643e6ebb2eaaf357972a6039ef5203061
nar3ndra <47317679+nar3ndra@users.noreply.github.com>
2020-04-25 05:44:59 +0530

    Solved

--- SquaresofaSortedArray.java
+++ SquaresofaSortedArray.java
@@ -1,5 +1,40 @@
 class Solution {
     public int[] sortedSquares(int[] A) {
+        int N = A.length;
+        int j = 0;
+        while (j < N && A[j] < 0)
+            j++;
+        int i = j-1;
+
+        int[] ans = new int[N];
+        int t = 0;
+
+        while (i >= 0 && j < N) {
+            if (A[i] * A[i] < A[j] * A[j]) {
+                ans[t++] = A[i] * A[i];
+                i--;
+            } else {
+                ans[t++] = A[j] * A[j];
+                j++;
+            }
+        }
+
+        while (i >= 0) {
+            ans[t++] = A[i] * A[i];
+            i--;
+        }
+        while (j < N) {
+            ans[t++] = A[j] * A[j];
+            j++;
+        }
+
+        return ans;
+    }
+}
+/*
+Inplace Array solution
+class Solution {
+    public int[] sortedSquares(int[] A) {
         int n=A.length;
         for(int i=0;i<n;i++){
             A[i]=A[i]*A[i];
@@ -9,3 +44,4 @@ class Solution {
         return A;
     }
 }
+*/
